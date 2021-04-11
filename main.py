08e2057c0419b05180f057e8b15717078e719630
Almog-Malka1 <almogmalkamain@gmail.com>
2021-04-11 17:32:29 +0300

    adding main GUI form.

--- main.py
+++ main.py
@@ -1,23 +1,75 @@
-import sys
+# ---- modules ----
 import getpass
-from tkinter import *
+import os
+import platform
 
+
+# ---- local vars ----
+main_txt = r"""
+  _                 _____          _____           _                 
+ | |               |_   _|        / ____|         | |                
+ | |     ___   __ _  | |  _ __   | (___  _   _ ___| |_ ___ _ __ ___  
+ | |    / _ \ / _` | | | | '_ \   \___ \| | | / __| __/ _ \ '_ ` _ \ 
+ | |___| (_) | (_| |_| |_| | | |  ____) | |_| \__ \ ||  __/ | | | | |
+ |______\___/ \__, |_____|_| |_| |_____/ \__, |___/\__\___|_| |_| |_|
+               __/ |                      __/ |                      
+              |___/                      |___/  
+                                               made by: @almog._.malka
+
+
+
+ What you are looking for?
+ 1. LogIn  
+ 2. SingUp
+ 3. Exit
+
+ Enter your option number.
+
+ """
+
+option = None
 
 # -----GUI setup-----
-root = Tk()
-root.title("Login Form")
-root.geometry('500x500')
+def main_screen():
+    #global vars
+    global option
 
-mainloop()
+    #show main screen
+    print(main_txt)
+    option = input("Enter your number: " )
+    option_list = ['1', '2', '3']
+
+    while option not in option_list:
+        print("Inviled input, use only in 1, 2, 3.\n")
+        option = input("Enter your number: " )
+
+    return option
+
+def login_form():
+    print("log in.")
+
+def singup_form():
+    print("sing up.")
 
 
 
+def main():
+    # global vars
+    global option
 
+    # funcs config
+    main_screen()
+    if option == '1':
+        login_form()
+    elif option == '2':
+        singup_form()
+    elif option == '3':
+        exit()
 
+main()
 
 
 #GUI MENU 
-
 
 #TAKE INFO
 
